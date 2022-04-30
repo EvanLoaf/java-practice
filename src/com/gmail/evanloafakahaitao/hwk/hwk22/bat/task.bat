e1c8f780b0000a3cd6bb297f0fc2624e4d0e9f73
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-30 20:20:40 +0300

    Add practice .BAT script with parameters

---
+++ src/com/gmail/evanloafakahaitao/hwk/hwk22/bat/task.bat
@@ -0,0 +1,12 @@
+set currdate=%1
+set folder=Screenshots_%currdate%
+mkdir %folder%
+move *.jpg %folder%
+move *.png %folder%
+cd %folder%
+dir /b *.jpg >> ..\screenshots.found
+dir /b *.png >> ..\screenshots.found
+set path="C:\Program Files\WinRAR\"
+rar a ..\screenshots
+cd..
+rmdir /s /q %folder%
--- src/com/gmail/evanloafakahaitao/hwk/hwk26/Main.java
+++ src/com/gmail/evanloafakahaitao/hwk/hwk26/Main.java
@@ -1,9 +1,17 @@
 package com.gmail.evanloafakahaitao.hwk.hwk26;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         CityApp cityApp = CityApp.getInstance();
         cityApp.runApp();
+
+        Pattern p = Pattern.compile("(?!.*?\\s{2,}.*?)[\\d\\sa-zA-Z]{1,100}");
+        String input = "hey  there";
+        Matcher matcher = p.matcher(input);
+        System.out.println(matcher.matches());
     }
 }
